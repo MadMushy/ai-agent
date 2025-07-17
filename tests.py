b925a628e2ad4c48a6f58c3ac742000da744e215
da5734 <davidhels92@proton.me>
2025-07-17 05:19:13 -0700

    finished read from file

--- tests.py
+++ tests.py
@@ -7,8 +7,20 @@ from functions.config import *
 
 class Testget_file_content(unittest.TestCase):
 
-    def test_current(self):
-        result = get_file_content("calculator", "lorem.txt")
+    def test_main(self):
+        result = get_file_content("calculator", "main.py")
+        print(result)
+
+    def test_pkg(self):
+        result = get_file_content("calculator", "pkg/calculator.py")
+        print(result)
+
+    def test_bin(self):
+        result = get_file_content("calculator", "/bin/cat")
+        print(result)
+
+    def test_non_exsist(self):
+        result = get_file_content("calculator", "pkg/does_not_exist.py")
         print(result)
 
 if __name__ == "__main__":
